761558be603a69fb340502aaef396c58013a2105
Ssid7647 <mkl9887647844@gmail.com>
2022-07-01 12:17:12 +0530

    edited app.js

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
-const port = 8080;
+const generateUUID=require('./translationModule/uuidGenerator')
+const getTranslation=require('./translationModule/translate')
+const generateFile=require('./translationModule/generateXmlFromDB').generateFile
+
+
+
+
+
+
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.post('/', async (req, res) => {
+    try {
+        let uuid = await generateUUID();
+        res.status(200).send({
+            "uuid": uuid
+        })
+        await getTranslation({ "body": req.body, "uuid": uuid })
+        res.status(200).send("ok done!!!");
+    } catch (error) {
+        console.log(error)
+        res.send(null)
+    }
+})
+app.post('/getFile', async (req, res) => {
+    try {
+        await generateFile(req.body.uuid)
+        res.status(200).send("ok done!!!");
+
+    } catch (error) {
+        res.send(error)
+    }
+})
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
